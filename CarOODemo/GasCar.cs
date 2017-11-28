07e5fe909042645003128d01548fe1754c7e5dfb
Eric Berger <eric@brrgrr.com>
2017-11-27 19:14:08 -0600

    Class 3.7 - Live coding

---
+++ CarOODemo/GasCar.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarOODemo
+{
+    public class GasCar : Car
+    {
+        public int GasTankSize { get; private set; }
+        public double GasTankLevel { get; private set; }
+        public double MilesPerGallon { get; private set; }
+
+        public GasCar(string make, string model, int gasTankSize, double milesPerGallon) : base(make, model)
+        {
+            this.GasTankSize = gasTankSize;
+            this.GasTankLevel = gasTankSize;
+            this.MilesPerGallon = milesPerGallon;
+        }
+
+        public override void Drive(int miles)
+        {
+            // adjust fuel based on MPG
+            // write out distance traveled and fuel remaining
+            double maxDistance = this.MilesPerGallon * this.GasTankLevel;
+            double distanceTraveled = miles;
+            if (miles > maxDistance)
+            {
+                distanceTraveled = maxDistance;
+            }
+            double gallonsUsed = distanceTraveled / this.MilesPerGallon;
+            this.GasTankLevel -= gallonsUsed;
+            var text = String.Format("Traveled {0} Miles, Used {1} Gallons, {2} Gallons Remaining ", distanceTraveled, gallonsUsed, this.GasTankLevel);
+            Console.WriteLine(text);
+    }
+    }
+}
--- CarOODemo/Program.cs
+++ CarOODemo/Program.cs
@@ -6,7 +6,19 @@ namespace CarOODemo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //TODO: create different cars and drive them
+            //Car car = new Car("Ford", "Prefect");
+            GasCar explorer = new GasCar("Ford", "Explorer", 20, 15 );
+            ElectricCar leaf = new ElectricCar("Nissan", "Leaf", 75, 3);
+            //Console.WriteLine(explorer.Make);
+            //Console.ReadKey();
+
+            explorer.Drive(75);
+            explorer.Drive(50);
+            leaf.Drive(200);
+            leaf.Drive(200);
+
+           
         }
     }
 }
